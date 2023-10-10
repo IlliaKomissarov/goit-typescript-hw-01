4adc0102f9633a53511fcf8ed5830b98f8c7bddd
Illia Komissarov <iliakomissarow99@gmail.com>
2023-10-10 16:20:00 +0300

    done

---
+++ ts_courses/dist/index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+if (button && input) {
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
+    });
+}
---
+++ ts_courses/src/index.ts
@@ -0,0 +1,8 @@
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
+
+if (button && input) {
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
+}
